38835b0f82c9b50cceac3b98808208526b2084a5
TawaGC <giucalvia99@gmail.com>
2024-06-29 17:24:30 +0200

    inizializazione

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { AlbumsListComponent } from './components/albums-list/albums-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '', component : WelcomeComponent
+  },
+  {
+    path : 'albums', component : AlbumsListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { AlbumsListComponent } from './components/albums-list/albums-list.component';
+import { AlbumDetailComponent } from './components/albums-list/album-detail/album-detail.component';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { CardComponent } from './components/cards-list/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    AlbumsListComponent,
+    AlbumDetailComponent,
+    CardsListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
